d9a3b371160a8304750390549a27840fea79d219
Pierre Grandjean <pierre.grandjean@gmail.com>
2017-01-08 21:23:53 +0100

    Neo4J integration

--- src/main/java/Setram.java
+++ src/main/java/Setram.java
@@ -24,8 +24,10 @@ import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
-import org.apache.commons.lang3.time.DateUtils;
 import org.jsoup.select.Elements;
+
+import org.neo4j.driver.v1.*;
+import static org.neo4j.driver.v1.Values.parameters;
 
 import java.net.URL;
 import java.text.ParseException;
@@ -44,12 +46,46 @@ public class Setram {
 
             get("/", (req, res) -> display() );
             get("/timetable/", (req, res) -> addToTimetable() );
+            get("/test/", (req, res) -> test() );
 
         } catch(Exception e){
             e.printStackTrace();
         }
 
     }
+
+    private static String test() {
+        return findTravelTime("Gare SNCF", "Saint-Martin", new Date());
+    }
+
+    private static String findTravelTime(String departure, String arrival, Date departureHour) {
+
+        Map<String,String> map = new HashMap<String,String>();
+        map.put("Université", "342|Universite|Le%20Mans|||437035|2337548|678!0;677!0;680!0;679!0;");
+        map.put("République", "318|R%E9publique|Le%20Mans|||440372|2335962|623!0;624!0;622!0;621!0;620!0;");
+        map.put("Gare SNCF", "206|Gare%20SNCF|Le%20Mans|||440083|2335103|393!0;390!0;391!0;392!0;394!0;395!0;");
+        map.put("Saint-Martin", "321|Saint%20Martin|Le%20Mans|||440979|2333984|632!0;629!0;631!0;630!0;");
+
+        String departureStopAreaName = "";
+        String arrivalStopAreaName = "";
+        if (map.containsKey(departure)) {
+            departureStopAreaName = map.get(departure);
+        }
+        if (map.containsKey(arrival)) {
+            arrivalStopAreaName = map.get(arrival);
+        }
+
+        String urlPart1 = "http://lemans.prod.navitia.com/Navitia/ITI_2_AnswersList.asp?DPoint=StopArea|";
+        String urlPart2 = "&APoint=StopArea|";
+        String urlPart3 = "&Date="; // Date format : yyyy|MM|d
+        String urlPart4 = "&Time=1|"; // Time format : h|m
+        String urlPart5 = "&Criteria=2||&Mode=|1|Pas%20de%20pr%E9f%E9rences|&HangDistance=400&Extend=1&TotOuTard=tot&DateFinBases=&DateMajBases=";
+
+        return "departureStopAreaName: " + departureStopAreaName + " ; arrivalStopAreaName: " + arrivalStopAreaName;
+
+    }
+
+
 
     private static String display() {
 
@@ -61,55 +97,79 @@ public class Setram {
 
         SimpleDateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         isoDateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
+
         SimpleDateFormat justDayDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         justDayDateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
         String day = justDayDateFormat.format(now);
 
-        SimpleDateFormat justDayDateFormatForSearchUrl = new SimpleDateFormat("yyyy|MM|d");
-        justDayDateFormatForSearchUrl.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
-        String dayForSearchUrl = justDayDateFormatForSearchUrl.format(now);
-
-        String searchUrlWithoutDate;
 
         String htmlTextToDisplay = "ERROR in display() method";
 
 
-        ArrayList<NameValuePair> realMinutesBeforeDepartures = new ArrayList<>();
-        NameValuePair params[] = new NameValuePair[3];
-        params[0] = new NameValuePair("a", "refresh");
+        htmlTextToDisplay = "<p>Prochains départ de Sécurité Sociale : <br/>";
 
-        // Securité Sociale - Bus 3 - Vers République
-        params[1] = new NameValuePair("refs", "268640008|268633352");
-        params[2] = new NameValuePair("ran", "524818637");
-        addRealMinutesBeforeDepartures("Bus 3 dir Oasis", realMinutesBeforeDepartures, params, now, isoDateFormat, day);
+        ArrayList<String> busLines = new ArrayList<>();
+        busLines.add("Bus 3 dir. Oasis - Centre des Expositions");
+        busLines.add("Bus 12 dir. République");
+        busLines.add("Bus 23 dir. République");
+        busLines.add("Bus 25 dir. République");
 
-        // Securité Sociale - Bus 12 - Vers République
-        params[1] = new NameValuePair("refs", "269260050");
-        params[2] = new NameValuePair("ran", "663034440");
-        addRealMinutesBeforeDepartures("Bus 12 dir République", realMinutesBeforeDepartures, params, now, isoDateFormat, day);
-
-        // Securité Sociale - Bus 23 - Vers République
-        params[1] = new NameValuePair("refs", "269979663");
-        params[2] = new NameValuePair("ran", "177820456");
-        addRealMinutesBeforeDepartures("Bus 23 dir République", realMinutesBeforeDepartures, params, now, isoDateFormat, day);
-
-        // Securité Sociale - Bus 25 - Vers République
-        params[1] = new NameValuePair("refs", "270112787|270112533|270111761|270111507");
-        params[2] = new NameValuePair("ran", "452935723");
-        addRealMinutesBeforeDepartures("Bus 25 dir République", realMinutesBeforeDepartures, params, now, isoDateFormat, day);
-
-        htmlTextToDisplay = "Prochains départ de Sécurité Sociale : <br/>";
-
-        // Sorting
-        Collections.sort(realMinutesBeforeDepartures, comp);
+        ArrayList<NameValuePair> realMinutesBeforeDepartures = readRealMinutesBeforeDepartures("Sécurité Sociale", busLines, now, isoDateFormat, day);
 
         for (NameValuePair realMinutesBeforeDeparture : realMinutesBeforeDepartures) {
             htmlTextToDisplay = htmlTextToDisplay + realMinutesBeforeDeparture.getValue() + " minutes (" + realMinutesBeforeDeparture.getName() + ")<br/>";
         }
 
 
+        htmlTextToDisplay = htmlTextToDisplay + "</p><p>Prochains départ de Californie : <br/>";
+
+        ArrayList<String> busLines2 = new ArrayList<>();
+        busLines.add("Bus 12 dir. République");
+        busLines.add("Bus 12 dir. Saint-Martin");
+
+        ArrayList<NameValuePair> realMinutesBeforeDepartures2 = readRealMinutesBeforeDepartures("Californie", busLines2, now, isoDateFormat, day);
+
+        for (NameValuePair realMinutesBeforeDeparture : realMinutesBeforeDepartures2) {
+            htmlTextToDisplay = htmlTextToDisplay + realMinutesBeforeDeparture.getValue() + " minutes (" + realMinutesBeforeDeparture.getName() + ")<br/>";
+        }
+
+        htmlTextToDisplay = htmlTextToDisplay + "</p>";
+
+
         return htmlTextToDisplay;
 
+    }
+
+
+    private static ArrayList<NameValuePair> readRealMinutesBeforeDepartures(String stopArea, ArrayList<String> busLines, Date now, SimpleDateFormat isoDateFormat, String day) {
+
+        ArrayList<NameValuePair> realMinutesBeforeDepartures = new ArrayList<>();
+        NameValuePair params[] = new NameValuePair[3];
+        params[0] = new NameValuePair("a", "refresh");
+
+        Driver driver = GraphDatabase.driver( System.getenv("GRAPHENEDB_BOLT_URL"), AuthTokens.basic( System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD") ) );
+        Session session = driver.session();
+
+        for (String busLine : busLines) {
+            StatementResult result = session.run("MATCH (b:Bus {name: {busLine}})-[r:STOPS_AT]->(s:Stop {name: {stopArea}}) " +
+                    "RETURN r.refs AS refs, r.ran AS ran",
+                    parameters("busLine", busLine, "stopArea", stopArea));
+            while ( result.hasNext() )
+            {
+                Record record = result.next();
+                params[1] = new NameValuePair("refs", record.get("refs").asString());
+                params[2] = new NameValuePair("ran", record.get("ran").asString());
+            }
+            addRealMinutesBeforeDepartures(busLine, realMinutesBeforeDepartures, params, now, isoDateFormat, day);
+        }
+
+        driver.close();
+        session.close();
+
+        // Sorting
+        Collections.sort(realMinutesBeforeDepartures, comp);
+
+        return realMinutesBeforeDepartures;
     }
 
 
@@ -118,6 +178,7 @@ public class Setram {
 
         SimpleDateFormat isoDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         isoDateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
+
         SimpleDateFormat justDayDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         justDayDateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
         String day = justDayDateFormat.format(now);
@@ -126,42 +187,59 @@ public class Setram {
         justDayDateFormatForSearchUrl.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
         String dayForSearchUrl = justDayDateFormatForSearchUrl.format(now);
 
-        String searchUrlWithoutDate;
+        Map<String,String> mapBusLines = new HashMap<String,String>();
+        mapBusLines.put("Tram 1 dir. Antarès-MMArena", "27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1");
+        mapBusLines.put("Bus 3 dir. Oasis - Centre des Expositions", "2|SET36|03|Gazonfier%20-%20Oasis|Gazonfier%20vers%20Oasis|Oasis%20vers%20Gazonfier|5|Bus&Direction=1");
+        mapBusLines.put("Bus 12 dir. Saint-Martin", "9|SET52|12|R%E9publique%20-%20St%20Martin|R%E9publique%20vers%20St%20Martin|St%20Martin%20vers%20R%E9publique|5|Bus&Direction=1");
 
-        String htmlTextToDisplay = "";
+        Map<String,String> mapStops = new HashMap<String,String>();
+        mapStops.put("Université", "342|SET1606|Universite|Le%20Mans");
+        mapStops.put("Gambetta-Mûriers", "204|SET1590|Gambetta-muriers|Le%20Mans");
+        mapStops.put("Éperon Cité Plantagenêt", "185|SET130|Eperon|Le%20Mans");
+        mapStops.put("République", "318|SET318|R%E9publique|Le%20Mans");
+        mapStops.put("Saint-Martin", "321|SET357|Saint%20Martin");
+        mapStops.put("Californie", "143|SET64|Californie|Le%20Mans");
+        mapStops.put("Sécurité Sociale", "338|SET344|S%E9curit%E9%20Sociale|Le%20Mans");
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Tram 1 direction Antarès-MMArena, Arrêt Université ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1%7CSTA1%7CSetram&Line=27%7CSET33%7CT1%7CAntar%E8s%20-%20Universit%E9%7CAntar%E8s%20vers%20Universit%E9%7CUniversit%E9%20vers%20Antar%E8s%7C16%7CTramway&Direction=-1&StopArea=342%7CSET1606%7CUniversite%7CLe%20Mans&Date=";
+        String searchUrlPart1BeforeBusLine = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=";
+        String searchUrlPart2BeforeStop = "&StopArea=";
+        String searchUrlPart3BeforeDate = "&Date=";
+
+        String busLine;
+        String stop;
+
+
+
+        //TODO : for each busLine, for each stop of this busLine :
+//        String searchUrlWithoutDate = searchUrlPart1BeforeBusLine + mapBusLines.get(busLine) + searchUrlPart2BeforeStop + mapStops.get(stop) + searchUrlPart3BeforeDate;
+//        captureTimetable(busLine, stop, searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
+
+
+/*
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1&StopArea=342|SET1606|Universite|Le%20Mans&Date=";
         captureTimetable("Tram 1 dir. Antarès-MMArena", "Université", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Tram 1 direction Antarès-MMArena, Arrêt Gambetta-Mûriers ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1%7CSTA1%7CSetram&Line=27%7CSET33%7CT1%7CAntar%E8s%20-%20Universit%E9%7CAntar%E8s%20vers%20Universit%E9%7CUniversit%E9%20vers%20Antar%E8s%7C16%7CTramway&Direction=-1&StopArea=204%7CSET1590%7CGambetta-muriers%7CLe%20Mans&Date=";
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1&StopArea=204|SET1590|Gambetta-muriers|Le%20Mans&Date=";
         captureTimetable("Tram 1 dir. Antarès-MMArena", "Gambetta-Mûriers", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Tram 1 direction Antarès-MMArena, Arrêt Éperon Cité Plantagenêt ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1%7CSTA1%7CSetram&Line=27%7CSET33%7CT1%7CAntar%E8s%20-%20Universit%E9%7CAntar%E8s%20vers%20Universit%E9%7CUniversit%E9%20vers%20Antar%E8s%7C16%7CTramway&Direction=-1&StopArea=185%7CSET130%7CEperon%7CLe%20Mans&Date=";
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1&StopArea=185|SET130|Eperon|Le%20Mans&Date=";
         captureTimetable("Tram 1 dir. Antarès-MMArena", "Éperon Cité Plantagenêt", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Tram 1 direction Antarès-MMArena, Arrêt République ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1%7CSTA1%7CSetram&Line=27%7CSET33%7CT1%7CAntar%E8s%20-%20Universit%E9%7CAntar%E8s%20vers%20Universit%E9%7CUniversit%E9%20vers%20Antar%E8s%7C16%7CTramway&Direction=-1&StopArea=318%7CSET318%7CR%E9publique%7CLe%20Mans&Date=";
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1&StopArea=318|SET318|R%E9publique|Le%20Mans&Date=";
         captureTimetable("Tram 1 dir. Antarès-MMArena", "République", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Tram 1 direction Antarès-MMArena, Arrêt Saint-Martin ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1%7CSTA1%7CSetram&Line=27%7CSET33%7CT1%7CAntar%E8s%20-%20Universit%E9%7CAntar%E8s%20vers%20Universit%E9%7CUniversit%E9%20vers%20Antar%E8s%7C16%7CTramway&Direction=-1&StopArea=321%7CSET357%7CSaint%20Martin%7CLe%20Mans&Date=";
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=27|SET33|T1|Antar%E8s%20-%20Universit%E9|Antar%E8s%20vers%20Universit%E9|Universit%E9%20vers%20Antar%E8s|16|Tramway&Direction=-1&StopArea=321|SET357|Saint%20Martin|Le%20Mans&Date=";
         captureTimetable("Tram 1 dir. Antarès-MMArena", "Saint-Martin", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
 
-        htmlTextToDisplay = htmlTextToDisplay + "Recording Bus 12 direction Antarès-MMArena, Arrêt Californie ...<br/>";
-        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=9|SET52|12|R%EF%BF%BDpublique%20-%20St%20Martin|R%EF%BF%BDpublique%20vers%20St%20Martin|St%20Martin%20vers%20R%EF%BF%BDpublique|5|Bus&Direction=1&StopArea=143|SET64|Californie|Le%20Mans&Date=";
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=9|SET52|12|R%E9publique%20-%20St%20Martin|R%E9publique%20vers%20St%20Martin|St%20Martin%20vers%20R%E9publique|5|Bus&Direction=1&StopArea=143|SET64|Californie|Le%20Mans&Date=";
         captureTimetable("Bus 12 dir. Saint-Martin", "Californie", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
-        htmlTextToDisplay = htmlTextToDisplay + "... done<br/>";
 
-        return htmlTextToDisplay;
+        searchUrlWithoutDate = "http://lemans.prod.navitia.com/Navitia/HP_4_HP.asp?Network=1|STA1|Setram&Line=2|SET36|03|Gazonfier%20-%20Oasis|Gazonfier%20vers%20Oasis|Oasis%20vers%20Gazonfier|5|Bus&Direction=1&StopArea=338|SET344|S%E9curit%E9%20Sociale|Le%20Mans&Date=";
+        captureTimetable("Bus 3 dir. Oasis - Centre des Expositions", "Sécurité Sociale", searchUrlWithoutDate, isoDateFormat, day, dayForSearchUrl);
+*/
+
+        return "Capture terminée";
 
     }
 
@@ -227,7 +305,7 @@ public class Setram {
 
     private static void captureTimetable(String busLine, String stop, String searchUrlWithoutDate, SimpleDateFormat isoDateFormat, String day, String dayForSearchUrl) throws IOException, ParseException {
 
-        DBCollection myCollection = connectToDB("timetable");
+        DBCollection myCollection = connectToMongoDB("timetable");
 
         Document doc = Jsoup.connect(searchUrlWithoutDate + dayForSearchUrl).get();
 
@@ -259,7 +337,7 @@ public class Setram {
     }
 
 
-    private static DBCollection connectToDB(String collectionName) {
+    private static DBCollection connectToMongoDB(String collectionName) {
         DBCollection collection = null;
         try {
             MongoClientURI uri = new MongoClientURI(System.getenv("MONGODB_URI"));
