47a12735bdb0da0212b4ca55da75307c4adee975
Pierre Grandjean <pierre.grandjean@gmail.com>
2017-02-01 22:23:02 +0100

    Starting refactoring of Neo4J access.

--- src/main/java/Setram.java
+++ src/main/java/Setram.java
@@ -47,6 +47,14 @@ public class Setram {
 
     static GraphDatabaseService graphDb;
 
+    private static final RelationshipType NEXT = RelationshipType.withName( "NEXT" );
+    private static final RelationshipType STOPS_AT = RelationshipType.withName( "STOPS_AT" );
+
+    private static final Label STOP = Label.label( "Stop" );
+    private static final Label BUS = Label.label( "Bus" );
+    private static final Label TRAM = Label.label( "Tram" );
+
+
     public static void main(String[] args) {
 
         File data = new File("data");
@@ -56,11 +64,11 @@ public class Setram {
         port(Integer.valueOf(System.getenv("PORT")));
 
         try {
-/*
             get("/", (req, res) -> display() );
+/*
             get("/timetable/", (req, res) -> addToTimetable() );
-*/
             get("/test/", (req, res) -> test3() );
+*/
             get("/init/", (req, res) -> initNeo4jDb() );
             get("/read/", (req, res) -> readNeo4jDb());
             get("/donotsleep/", (req, res) -> donotsleep() );
@@ -74,53 +82,6 @@ public class Setram {
 
     private static String donotsleep() {
         return "I'm awake!";
-    }
-
-
-    private static enum RelTypes implements RelationshipType
-    {
-        KNOWS
-    }
-
-    public enum Labels implements Label {
-        Stop,
-        Bus,
-        Tram;
-    }
-
-    private static String test3() {
-        System.out.println("test3.1");
-        try ( Transaction tx = graphDb.beginTx() )
-        {
-            // Database operations go here
-
-            System.out.println("test3.2");
-
-            Node firstNode = graphDb.createNode();
-            firstNode.setProperty( "message", "Hello, " );
-            Node secondNode = graphDb.createNode();
-            secondNode.setProperty( "message", "World!" );
-
-            Relationship relationship = firstNode.createRelationshipTo( secondNode, RelTypes.KNOWS );
-            relationship.setProperty( "message", "brave Neo4j " );
-
-            System.out.print( firstNode.getProperty( "message" ) );
-            System.out.print( relationship.getProperty( "message" ) );
-            System.out.print( secondNode.getProperty( "message" ) );
-
-            tx.success();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        try {
-            String content = readFile("Neo4j-data.cypher", Charset.defaultCharset());
-            return content;
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        return "OK";
     }
 
 
@@ -164,7 +125,7 @@ public class Setram {
         try ( Transaction tx = graphDb.beginTx() )
         {
             // Find all Stops
-            ResourceIterator<Node> stops = graphDb.findNodes(Labels.Stop);
+            ResourceIterator<Node> stops = graphDb.findNodes(STOP);
             System.out.println( "Stops:" );
             while( stops.hasNext() )
             {
@@ -198,7 +159,7 @@ public class Setram {
         } );
     }
 
-/*
+
 
     private static String addMinutesToNextStop(String busLine, String departure, String arrival, DBCollection myCollection) {
 
@@ -269,7 +230,7 @@ public class Setram {
     }
 
 
-
+/*
     private static String test2() {
 
         try {
@@ -313,23 +274,9 @@ public class Setram {
 
     private static String test() {
 //        return findTravelTime("Gare SNCF", "Saint-Martin", new Date());
-
-        users = graphDB.findNodes( Labels.USER );
-        System.out.println("Users:");
-        while( users.hasNext() )
-        {
-            Node user = users.next();
-            System.out.print( "\t" + user.getProperty( "name" ) + " has seen " );
-            for( Relationship relationship : user.getRelationships(
-                    RelationshipTypes.HAS_SEEN ) )
-            {
-                Node movie = relationship.getOtherNode( user );
-                System.out.print( "\t" + movie.getProperty( "name" ) );
-            }
-            System.out.println();
-        }
-
     }
+
+*/
 
     private static String findTravelTime(String departure, String arrival, Date departureHour) {
 
@@ -434,24 +381,38 @@ public class Setram {
         NameValuePair params[] = new NameValuePair[3];
         params[0] = new NameValuePair("a", "refresh");
 
-        Driver driver = GraphDatabase.driver( System.getenv("GRAPHENEDB_BOLT_URL"), AuthTokens.basic( System.getenv("GRAPHENEDB_BOLT_USER"), System.getenv("GRAPHENEDB_BOLT_PASSWORD") ) );
-        Session session = driver.session();
+        try ( Transaction tx = graphDb.beginTx() )
+        {
+            for (String busLine : busLines) {
+                System.out.println(busLine);
+                System.out.println(stopArea);
 
-        for (String busLine : busLines) {
-            StatementResult result = session.run("MATCH (b {name: {busLine}})-[r:STOPS_AT]->(s:Stop {name: {stopArea}}) " +
-                    "RETURN r.refs AS refs, r.ran AS ran",
-                    parameters("busLine", busLine, "stopArea", stopArea));
-            while ( result.hasNext() )
-            {
-                Record record = result.next();
-                params[1] = new NameValuePair("refs", record.get("refs").asString());
-                params[2] = new NameValuePair("ran", record.get("ran").asString());
+                Node busNode = graphDb.findNode(BUS, "name", busLine);
+                if (busNode == null) {
+                    busNode = graphDb.findNode(TRAM, "name", busLine);
+                }
+                Node stopNode = graphDb.findNode(STOP, "name", stopArea);
+
+                Iterable<Relationship> relationships = busNode.getRelationships(STOPS_AT, Direction.OUTGOING);
+                for( Relationship relationship : relationships )
+                {
+                    if (relationship.getOtherNode(busNode).equals(stopNode)) {
+                        System.out.println("refs = " + relationship.getProperty("refs").toString());
+                        System.out.println("ran = " + relationship.getProperty("ran").toString());
+                        params[1] = new NameValuePair("refs", relationship.getProperty("refs").toString());
+                        params[2] = new NameValuePair("ran", relationship.getProperty("ran").toString());
+
+                    }
+                }
+
+                addRealMinutesBeforeDepartures(busLine, realMinutesBeforeDepartures, params, now, isoDateFormat, day);
+
             }
-            addRealMinutesBeforeDepartures(busLine, realMinutesBeforeDepartures, params, now, isoDateFormat, day);
-        }
+            tx.success();
 
-        driver.close();
-        session.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         // Sorting
         Collections.sort(realMinutesBeforeDepartures, comp);
@@ -459,7 +420,7 @@ public class Setram {
         return realMinutesBeforeDepartures;
     }
 
-
+/*
     private static String addToTimetable() throws IOException, ParseException {
 
         // We open MongoDB and Neo4J connections
@@ -526,7 +487,7 @@ public class Setram {
         return "Capture terminée";
 
     }
-
+*/
 
     private static ArrayList<NameValuePair> addRealMinutesBeforeDepartures(String busLine, ArrayList<NameValuePair> realMinutesBeforeDepartures, NameValuePair params[], Date now, SimpleDateFormat isoDateFormat, String day) {
 
@@ -631,6 +592,6 @@ public class Setram {
         }
         return collection;
     }
-*/
+
 
 }
